e606c35df87233480d8ce9e5dec0f276d7df65f2
HoangLong9991 <longnhgcd191143@fpt.edu.vn>
2022-08-01 00:58:57 +0700

    Add ApplicationUser and Edit register to use ApplicationUser

---
+++ HoangLongStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HoangLongStore.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HoangLongStore.Areas.Identity.IdentityHostingStartup))]
+namespace HoangLongStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- HoangLongStore/Data/Migrations/20220731171415_customIdentityUser.Designer.cs
+++ HoangLongStore/Data/Migrations/20220731171415_customIdentityUser.Designer.cs
@@ -4,14 +4,16 @@ using HoangLongStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HoangLongStore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220731171415_customIdentityUser")]
+    partial class customIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
