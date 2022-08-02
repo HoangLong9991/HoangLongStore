aedf691256b75846753eaae1aca564362f3e7dd7
HoangLong9991 <longnhgcd191143@fpt.edu.vn>
2022-08-03 00:10:59 +0700

    Add method edit product

--- HoangLongStore/Controllers/ProductsController.cs
+++ HoangLongStore/Controllers/ProductsController.cs
@@ -25,5 +25,47 @@ namespace HoangLongStore.Controllers
 
 			return View(result);
 		}
+
+		[Authorize (Roles = "admin")]
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Product product)
+		{
+			if(!ModelState.IsValid) return View(product);
+			
+			var result = context.Products.Add(product);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		[Authorize(Roles = "admin")]
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			var product = context.Products.SingleOrDefault(t => t.Id == id);
+			if (product is null)
+			{
+				return NotFound();
+			}
+			return View(product);
+		}
+		[HttpPost]
+		public IActionResult Edit(Product product)
+		{
+			var productInDb = context.Products.FirstOrDefault(t => t.Id == product.Id);
+
+				productInDb.NameProduct = product.NameProduct;
+				productInDb.QuantityProduct += product.QuantityProduct;
+				productInDb.PriceProduct = product.PriceProduct;
+				productInDb.DescriptionProduct = product.DescriptionProduct;
+				
+				context.SaveChanges();
+				return RedirectToAction("Index");
+		}
+
 	}
 }
